229db6c8dac3c0251d1c059e933f01eccdd25fd3
iipmnigeria <43499291+iipmnigeria@users.noreply.github.com>
2026-07-20 16:06:16 +0100

    Mount protected Super Admin assignment widget

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AdminAssignmentWidget from './components/AdminAssignmentWidget';
 import SupabaseSessionBoundary from './components/SupabaseSessionBoundary';
 import './index.css';
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SupabaseSessionBoundary>
       <App />
+      <AdminAssignmentWidget />
     </SupabaseSessionBoundary>
   </StrictMode>,
 );
