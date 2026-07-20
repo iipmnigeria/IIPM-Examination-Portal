2e1794cd4c875f1002f22a31a350a9582959607a
iipmnigeria <43499291+iipmnigeria@users.noreply.github.com>
2026-07-20 16:01:43 +0100

    Use Supabase session boundary without demo adapter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './githubPagesDemo';
 import App from './App.tsx';
 import SupabaseSessionBoundary from './components/SupabaseSessionBoundary';
 import './index.css';
