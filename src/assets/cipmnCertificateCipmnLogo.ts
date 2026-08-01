460290230e597e29c287b726a6cabce1fa9b80da
iipmnigeria <43499291+iipmnigeria@users.noreply.github.com>
2026-08-01 12:08:18 +0100

    Wire verified CIPMN PNG asset

--- src/assets/cipmnCertificateCipmnLogo.ts
+++ src/assets/cipmnCertificateCipmnLogo.ts
@@ -1,15 +1,23 @@
-import { CIPMN_PRINT_PNG_CHUNK_1 } from './cipmnCertificatePrint/cipmn1';
-import { CIPMN_PRINT_PNG_CHUNK_2 } from './cipmnCertificatePrint/cipmn2';
-import { CIPMN_PRINT_PNG_CHUNK_3 } from './cipmnCertificatePrint/cipmn3';
-import { CIPMN_PRINT_PNG_CHUNK_4 } from './cipmnCertificatePrint/cipmn4';
-import { CIPMN_PRINT_PNG_CHUNK_5 } from './cipmnCertificatePrint/cipmn5';
-import { CIPMN_PRINT_PNG_CHUNK_6 } from './cipmnCertificatePrint/cipmn6';
+import { CIPMN_PRINT_PNG_V2_CHUNK_01 } from './cipmnCertificatePrint/cipmnV2_01';
+import { CIPMN_PRINT_PNG_V2_CHUNK_02 } from './cipmnCertificatePrint/cipmnV2_02';
+import { CIPMN_PRINT_PNG_V2_CHUNK_03 } from './cipmnCertificatePrint/cipmnV2_03';
+import { CIPMN_PRINT_PNG_V2_CHUNK_04 } from './cipmnCertificatePrint/cipmnV2_04';
+import { CIPMN_PRINT_PNG_V2_CHUNK_05 } from './cipmnCertificatePrint/cipmnV2_05';
+import { CIPMN_PRINT_PNG_V2_CHUNK_06 } from './cipmnCertificatePrint/cipmnV2_06';
+import { CIPMN_PRINT_PNG_V2_CHUNK_07 } from './cipmnCertificatePrint/cipmnV2_07';
+import { CIPMN_PRINT_PNG_V2_CHUNK_08 } from './cipmnCertificatePrint/cipmnV2_08';
+import { CIPMN_PRINT_PNG_V2_CHUNK_09 } from './cipmnCertificatePrint/cipmnV2_09';
+import { CIPMN_PRINT_PNG_V2_CHUNK_10 } from './cipmnCertificatePrint/cipmnV2_10';
 
 export const CIPMN_CERTIFICATE_LOGO_DATA_URI =
   'data:image/png;base64,' +
-  CIPMN_PRINT_PNG_CHUNK_1 +
-  CIPMN_PRINT_PNG_CHUNK_2 +
-  CIPMN_PRINT_PNG_CHUNK_3 +
-  CIPMN_PRINT_PNG_CHUNK_4 +
-  CIPMN_PRINT_PNG_CHUNK_5 +
-  CIPMN_PRINT_PNG_CHUNK_6;
+  CIPMN_PRINT_PNG_V2_CHUNK_01 +
+  CIPMN_PRINT_PNG_V2_CHUNK_02 +
+  CIPMN_PRINT_PNG_V2_CHUNK_03 +
+  CIPMN_PRINT_PNG_V2_CHUNK_04 +
+  CIPMN_PRINT_PNG_V2_CHUNK_05 +
+  CIPMN_PRINT_PNG_V2_CHUNK_06 +
+  CIPMN_PRINT_PNG_V2_CHUNK_07 +
+  CIPMN_PRINT_PNG_V2_CHUNK_08 +
+  CIPMN_PRINT_PNG_V2_CHUNK_09 +
+  CIPMN_PRINT_PNG_V2_CHUNK_10;
